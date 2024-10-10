16f4101e56c613151baf4db520565dda80155aa8
Jimmy Wong <102245488+agi-singularity@users.noreply.github.com>
2024-10-10 10:40:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,36 @@ st.write(
     "Boost and Pruning"
 )
 
+#######################
+# Import libraries
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+
+#######################
+# Page configuration
+st.set_page_config(
+    page_title="Embeddings Analyzer",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+
+#######################
+# Load data
+# df = pd.read_csv('data/nwu_inference_slim.csv')
+# #df = df_reshaped
+
+#######################
+# Sidebar
+with st.sidebar:
+    
+    color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
+    selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+
+
